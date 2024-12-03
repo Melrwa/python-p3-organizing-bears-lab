30447982e1be50306fcf40c13e5109fc8a354ed2
melrwa <orwamelkizedek@gmail.com>
2024-12-03 09:10:44 +0300

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 5, 'M', 'Black', 'Energetic', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 7, 'F', 'Golden', 'Gentle', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 8, 'M', 'Brown', 'Serious', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 4, 'F', 'White', 'Playful', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 6, 'M', 'Green', 'Grumpy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 3, 'F', 'Black', 'Shy', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 2, 'M', 'Brown', 'Curious', 1);
